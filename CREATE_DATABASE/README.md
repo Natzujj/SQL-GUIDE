640ea05d9817f003ce92cabc84db0b95e8cb4a13
Natzujj <gabrielnatsujake@gmail.com>
2024-01-02 13:50:14 -0300

    Updating Readme

---
+++ CREATE_DATABASE/README.md
@@ -0,0 +1,13 @@
+# SQL Basics: Creating, Selecting, and Inserting
+
+## Overview
+Welcome to the SQL Basics section! In this module, we'll cover fundamental SQL operations, focusing on creating databases, tables, and inserting values. Let's dive into the key clauses and commands you'll encounter in this session.
+
+## Key Clauses
+- `CREATE`: The `CREATE` clause is used to create databases, tables, or other database objects. In this section, you'll explore how to craft the structure of your databases and tables.
+
+- `INSERT INTO`: The `INSERT INTO` clause is essential for adding data to your tables. Learn how to insert values into specific columns and create a well-organized dataset.
+
+- `VALUES`: The `VALUES` clause is paired with `INSERT INTO` to specify the actual data being inserted into a table. Discover the flexibility of inserting single or multiple rows with different values.
+
+### Feel free to explore and experiment with these clauses and commands to solidify your understanding of basic SQL operations. Happy querying!
--- OPERATORS/LOGICAL_AND_COMPARISON_OPERATORS.sql
+++ OPERATORS/LOGICAL_AND_COMPARISON_OPERATORS.sql
@@ -1,41 +1,55 @@
 -- Selecionando o banco de dados
-USE BibliotecaDB
+-- Selecting the database
+USE BibliotecaDB;
 
 -- Selecionando TODOS os livros
+-- Selecting ALL books
 SELECT * FROM Livros;
 
 -- Selecionando livros que foram publicados após 1080
+-- Selecting books published after 1080
 SELECT * FROM Livros l WHERE AnoPublicacao > 1980;
 
 -- Selecionando livros que foram publicados em um ano diferente de 1980
+-- Selecting books published in a year different from 1980
 SELECT * FROM Livros l WHERE AnoPublicacao <> 1980;
 
 -- Listagem de nacionalidades distintas dos autores
+-- Listing distinct nationalities of authors
 SELECT DISTINCT Nacionalidade FROM Autores a;
 
 -- Selecionando livros que foram publicados após 1980 ou Do autor Gabriel Garcia
+-- Selecting books published after 1980 or by author Gabriel Garcia
 SELECT * FROM Livros l WHERE (AnoPublicacao > 1980) OR (AutorID = 1);
 
 -- Selecionando livros que foram publicados após 1980 E pertence ao autor Gabriel Garcia
+-- Selecting books published after 1980 AND belonging to author Gabriel Garcia
 SELECT * FROM Livros l WHERE (AnoPublicacao > 1980) AND (AutorID = 1);
 
 -- Selecionando Autores cujo o ID seja MAIOR que TODOS os existentes nos autores ligados a tabela Livros
+-- Selecting Authors whose ID is GREATER than ALL existing IDs in the authors linked to the Livros table
 SELECT Nome FROM Autores WHERE AutorID > ALL (SELECT AutorID FROM Livros);
 
 -- Selecionando Autores cujo o ID seja maior que pelo menos um AutorID dos Livros
+-- Selecting Authors whose ID is greater than at least one AutorID from Livros
 SELECT Nome FROM Autores WHERE AutorID > ANY (SELECT AutorID FROM Livros);
 
 -- Selecionando registros dentro de um intervalo especificado
+-- Selecting records within a specified range
 SELECT Titulo, AnoPublicacao FROM Livros l WHERE AnoPublicacao BETWEEN 2000 AND 2020;
 
--- Selecionando registros que possuem pelo menos 1 livro associado a um autor 
+-- Selecionando registros que possuem pelo menos 1 livro associado a um autor
+-- Selecting records that have at least 1 book associated with an author
 SELECT Nome FROM Autores a WHERE EXISTS (SELECT 1 FROM Livros l WHERE AutorID = a.AutorID);
 
 -- Selecionando registro que possuem termos especificados em qualquer posição
+-- Selecting records that have specified terms in any position
 SELECT Titulo FROM Livros l WHERE Titulo LIKE '%Potter%';
 
 -- Selecionando registro que NÃO possuem termos especificados
+-- Selecting records that do NOT have specified terms
 SELECT Titulo, AnoPublicacao FROM Livros l WHERE NOT Genero = 'Mistério';
 
 -- Selecionando registro que possuem alguma correspondencia com a sub-consulta (Semelhante ao ANY)
+-- Selecting records that have some correspondence with the subquery (Similar to ANY)
 SELECT Nome FROM Autores a WHERE AutorID = SOME (SELECT AutorID FROM LIVROS WHERE AnoPublicacao > 2000);
